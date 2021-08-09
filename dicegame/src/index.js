ae7fb8c52cc4cdd4477ce358f6fe3167bb16a907
mandos1995 <mandos19950620@gmail.com>
2021-08-09 23:45:14 +0900

    💄 컴포넌트 추가

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
+import App from './App'
 
 ReactDOM.render(
-  <h1>안녕 리엑트!</h1>,
-  document.getElementById('root')
+  <App />
+  ,document.getElementById('root')
 );
